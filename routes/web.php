dafed51f19174bce1b1ba02ad35109e98457fc95
Arthur Fabiano <arthur.masterdevelop@gmail.com>
2023-04-16 18:53:22 -0300

    feat(Settings): Settings
    Initial Setting

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
